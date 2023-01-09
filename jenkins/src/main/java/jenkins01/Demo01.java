8508d3d94e39d714d60a6efa48335c944a13f535
Aditya <Aditya@Laptop-Aditya>
2023-01-09 15:54:06 +0530

    Today 1st commit

---
+++ jenkins/src/main/java/jenkins01/Demo01.java
@@ -0,0 +1,14 @@
+package jenkins01;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo01 {
+	@Test
+	public void tc1()
+	{
+		Reporter.log("Hii",true);
+	}
+
+}
---
+++ jenkins/src/main/java/jenkins01/Demo02.java
@@ -0,0 +1,14 @@
+package jenkins01;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo02 {
+	@Test
+	public void tc2()
+	{
+		Reporter.log("Hello",true);
+	}
+	
+}
